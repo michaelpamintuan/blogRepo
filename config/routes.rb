bba88cd5e6bb21b2fa4215da2f1a679da217ae47
michaelpamintuan <michael@paralejasconsulting.com>
2022-12-06 16:55:14 +0800

    Modified Files and Creating New Files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+    end
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-end
+
